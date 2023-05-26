6c2ac2d8e0c5dccc2596c413b4b9dfe962c5c031
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2023-05-26 12:02:19 +0530

    Fixing camera

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import time
 app = Flask(__name__)
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 detector=HandDetector(maxHands=1)
 classifier=Classifier("model/keras_model.h5","model/labels.txt")
 offset=20
