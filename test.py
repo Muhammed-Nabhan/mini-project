b9a09710149ad8f4c4f23d2aaf0de7e69c745b80
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2023-05-26 15:33:53 +0530

    Fixing camera using javascript API

--- test.py
+++ test.py
@@ -9,64 +9,64 @@ import time
 app = Flask(__name__)
 
 
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture(0)
 detector=HandDetector(maxHands=1)
 classifier=Classifier("model/keras_model.h5","model/labels.txt")
 offset=20
 imgSize=300
-folder="data/C"
-counter=0
 labels=["A","B","C"]
-
-def generate_frames():
-     while(cap.isOpened()):
-            ret,frame=cap.read()
-            imgOutput=frame.copy()
-            hands,img=detector.findHands(frame)
-            if hands:
-                hand=hands[0]
-                x,y,w,h=hand['bbox']
-
-                imgWhite=np.ones((imgSize,imgSize,3),np.uint8)*255
-                imgCrop=frame[y-offset:y+h+offset,x-offset:x+w+offset]
-
-                imgCropShape=imgCrop.shape
-
-
-                aspectRatio=h/w
-                if aspectRatio>1:
-                    k=imgSize/h
-                    wCal=math.ceil(k*w)
-                    imgResize=cv2.resize(imgCrop,(wCal,imgSize))
-                    imgResizeShape=imgResize.shape
-                    wGap=math.ceil((imgSize-wCal)/2)
-                    imgWhite[:,wGap:wCal+wGap]=imgResize
-                    prediction,index=classifier.getPrediction(imgWhite)
-                    print(prediction,index)
-
-
-                else:
-                    k=imgSize/w
-                    hCal=math.ceil(k*h)
-                    imgResize=cv2.resize(imgCrop,(imgSize,hCal))
-                    imgResizeShape=imgResize.shape
-                    hGap=math.ceil((imgSize-hCal)/2)
-                    imgWhite[hGap:hCal+hGap,:]=imgResize
-                    prediction,index=classifier.getPrediction(imgWhite)
-
-                cv2.putText(imgOutput,labels[index],(x,y-20),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),2)
-                    
-            ret, buffer =cv2.imencode('.jpg',imgOutput)
-            frame_bytes=buffer.tobytes()
-
-            yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' +frame_bytes + b'\r\n\r\n')
 @app.route('/')
 def index():
-        return render_template('index.html')
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace;boundary=frame')
+    return render_template('index.html')
+@app.route('/process_image',methods=['POST'])
+def process_image():
+    image_data=request.json['image']
+    image=base64.b64decode(image_data.split(',')[1])
+    image=Image.open(io.BytesIO(image))
+    frame=np.array(image)
+    frame=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+
+while(cap.isOpened()):
+    ret,frame=cap.read()
+    imgOutput=frame.copy()
+    hands,img=detector.findHands(frame)
+    if hands:
+        hand=hands[0]
+        x,y,w,h=hand['bbox']
+
+        imgWhite=np.ones((imgSize,imgSize,3),np.uint8)*255
+        imgCrop=frame[y-offset:y+h+offset,x-offset:x+w+offset]
+
+        imgCropShape=imgCrop.shape
+
+
+        aspectRatio=h/w
+        if aspectRatio>1:
+            k=imgSize/h
+            wCal=math.ceil(k*w)
+            imgResize=cv2.resize(imgCrop,(wCal,imgSize))
+            imgResizeShape=imgResize.shape
+            wGap=math.ceil((imgSize-wCal)/2)
+            imgWhite[:,wGap:wCal+wGap]=imgResize
+            prediction,index=classifier.getPrediction(imgWhite)
+            print(prediction,index)
+
+
+        else:
+            k=imgSize/w
+            hCal=math.ceil(k*h)
+            imgResize=cv2.resize(imgCrop,(imgSize,hCal))
+            imgResizeShape=imgResize.shape
+            hGap=math.ceil((imgSize-hCal)/2)
+            imgWhite[hGap:hCal+hGap,:]=imgResize
+            prediction,index=classifier.getPrediction(imgWhite)
+
+        cv2.putText(imgOutput,labels[index],(x,y-20),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),2)
+                    
+    _,buffer =cv2.imencode('.jpg',imgOutput)
+    image_data=base64.b64encode(buffer).decode('utf-8')
+    return jsonify(prediction=labels[index],image_data=image_data)
+
 if __name__ == '__main__':
     app.run(debug=True)
                 #cv2.imshow("ImageCrop",imgCrop)
